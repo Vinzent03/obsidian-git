0795c1a9e7b7e982403be658269ca0fbbd35f73b
Denis Olehov <denolehov@gmail.com>
2020-10-28 19:07:08 +0200

    chore: indent everything with 4 spaces

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,13 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "main.ts",
-  output: {
-    dir: ".",
-    sourcemap: "inline",
-    format: "cjs",
-    exports: "default",
-  },
-  external: ["obsidian"],
-  plugins: [typescript(), nodeResolve({ browser: true }), commonjs()],
+    input: "main.ts",
+    output: {
+        dir: ".",
+        sourcemap: "inline",
+        format: "cjs",
+        exports: "default",
+    },
+    external: ["obsidian"],
+    plugins: [typescript(), nodeResolve({ browser: true }), commonjs()],
 };
