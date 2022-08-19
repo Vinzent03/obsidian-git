aa5fa37579903243f6623fa99592203c76cd5478
Vinzent <vinzent03@proton.me>
2022-08-20 01:31:05 +0200

    fix: export mock IsomorphicGit.ts
    close #281

--- src/mocks/isomorphicGit.ts
+++ src/mocks/isomorphicGit.ts
@@ -1,6 +1,6 @@
 //@ts-nocheck
 // This file replaces the real src/isomorphicGit.ts file in the release build for obsidian-git plugin to reduce package size.
 import { GitManager } from "src/gitManager";
-class IsomorphicGit extends GitManager {
+export class IsomorphicGit extends GitManager {
 
 }
