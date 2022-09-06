7383da0b3adfdc292af6e839a619c68c92d1037d
Vinzent <vinzent03@proton.me>
2022-09-06 20:28:22 +0200

    ci: fix release workflow

--- src/mocks/isomorphicGit.ts
+++
@@ -1,6 +0,0 @@
-//@ts-nocheck
-// This file replaces the real src/isomorphicGit.ts file in the release build for obsidian-git plugin to reduce package size.
-import { GitManager } from "src/gitManager";
-export class IsomorphicGit extends GitManager {
-
-}
