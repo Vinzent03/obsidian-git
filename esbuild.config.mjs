255393db116e32cf77251e59c482da72f749e0e0
Vinzent <vinzent03@proton.me>
2022-09-15 14:13:34 +0200

    chore: upgrade target

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -21,7 +21,7 @@ esbuild.build({
     external: ['obsidian', 'electron','child_process','fs','path'],
     format: 'cjs',
     watch: !prod,
-    target: 'es2016',
+    target: 'es2018',
     logLevel: "info",
     sourcemap: prod ? false : 'inline',
     treeShaking: true,
