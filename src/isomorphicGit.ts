a0b9f21c08fec2bedf4870bb7c612dd52983ed82
tamiroh <tamiroh71@gmail.com>
2023-03-02 00:15:46 +0900

    Fix diff view issue on smartphones and tablets (#448)
    * Fix
    
    * Fix
    
    * Revert "Fix"
    
    This reverts commit b97c972232f82766cf62616c80a54670c174b170.
    
    * Revert "Fix"
    
    This reverts commit 986b9aaff0a7700f938a41598952d6d07b4cb92e.
    
    * Fix
    
    * style: run formatter
    
    ---------
    
    Co-authored-by: Vinzent <vinzent03@proton.me>

--- src/isomorphicGit.ts
+++ src/isomorphicGit.ts
@@ -972,14 +972,26 @@ export class IsomorphicGit extends GitManager {
         const stagedContent = new TextDecoder().decode(stagedBlob);
 
         if (stagedChanges) {
-            const headBlob = await readBlob({
-                ...this.getRepo(),
-                filepath: filePath,
-                oid: await this.resolveRef("HEAD"),
-            });
-            const headContent = new TextDecoder().decode(headBlob.blob);
+            const headContent = await this.resolveRef("HEAD")
+                .then((oid) =>
+                    readBlob({
+                        ...this.getRepo(),
+                        filepath: filePath,
+                        oid: oid,
+                    })
+                )
+                .then((headBlob) => new TextDecoder().decode(headBlob.blob))
+                .catch((err) => {
+                    if (err instanceof git.Errors.NotFoundError)
+                        return undefined;
+                    throw err;
+                });
 
-            const diff = createPatch(filePath, headContent, stagedContent);
+            const diff = createPatch(
+                filePath,
+                headContent ?? "",
+                stagedContent
+            );
             return diff;
         } else {
             let workdirContent: string;
