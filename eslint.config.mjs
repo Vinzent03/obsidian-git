16716b028dc11cbc3c33ec46bf3a090a44a7c4af
Vinzent <vinzent03@proton.me>
2024-10-09 13:14:59 +0200

    refactor: upgrade packages and migrate to eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import svelteParser from "svelte-eslint-parser";
+import tsParser from "@typescript-eslint/parser";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginSvelte from "eslint-plugin-svelte";
+
+export default tseslint.config(
+    {
+        ignores: ["**/node_modules/", "**/main.js"],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...eslintPluginSvelte.configs["flat/prettier"],
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+        },
+    },
+    {
+        files: ["**/*.svelte"],
+        languageOptions: {
+            parser: svelteParser,
+            parserOptions: {
+                extraFileExtensions: [".svelte"],
+                parser: tsParser,
+            },
+        },
+        rules: {
+            "no-undef": "off",
+        },
+    }
+);
